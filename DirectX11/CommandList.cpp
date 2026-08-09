SpectrumQT/XXMI-Libs-Package

commit 9fc83bc0889b8982c04b6b7bbf189d62dc122bc5

Fixed regression in parsing of UTF-8 chars and hyphens in ResourceCopyTarget names
While UTF-8 in names of ResourceCopyTarget wasn't really supposed to be legal, but lack of strictness both in o.g. 3dm parsers and UTF-8 support implementation in XXMI DLL combined opened the way for modders to abuse. From now on it has to be supported to prevent "regressions".

Hyphens handling for ResourceCopyTarget by o.g. 3dm parsers is also quite... surprising. For example, `$var = ib-1` is allowed, while `$var = vs-cb0-1` is not. Original syntax also allows trailing hyphens for custom resources, so `$var = ResourceFoo- -1` is allowed, while `$var = ResourceFoo--1` is not. This behavior also has to be replicated.

DirectX11/CommandList.cpp
@@ -3624,6 +3624,30 @@ static inline bool is_identifier_char(wchar_t c, OptionalChars identifier_flags)
 	return false;
 }
 
+static inline bool is_operator_char(wchar_t c)
+{
+	switch (c)
+	{
+	case L'=':
+	case L'&':
+	case L'|':
+	case L'+':
+	case L'-':
+	case L'/':
+	case L'*':
+	case L'>':
+	case L'<':
+	case L'%':
+	case L'!':
+	case L'^':
+	case L'~':
+		return true;
+
+	default:
+		return false;
+	}
+}
+
 static size_t FindIdentifierTokenEnd(const std::wstring& str, const size_t start = 0, OptionalChars identifier_flags = OptionalChars::NONE)
 {
 	for (size_t i = start; i < str.size(); ++i)
@@ -3713,7 +3737,7 @@ private:
 				return false;
 
 			// Broad token-boundary check only. Actual syntax validation is performed later by the dedicated parser.
-			if (!is_identifier_char(c, OptionalChars::ALL))
+			if ((is_operator_char(c) && c != L'-') || iswspace(c))
 				return false;
 		}
 
@@ -3752,7 +3776,7 @@ static size_t FindResourceCopyTargetTokenEnd(const std::wstring& str, size_t sta
 	// Scans a resource copy target token and returns the end position after performing minimal syntax validation:
 	// 1. Ensures identifier characters match `[a-z_-.0-9]+`.
 	// 2. Ensures an optional namespace is properly closed, while allowing arbitrary characters in the namespace name.
-	// 3. Ensures optional bracket expressions are properly balanced while allowing arbitrary characters inside them.
+	// 3. Ensures optional bracket expressions are properly nested while allowing arbitrary characters inside them.
 	// 4. Recognizes the optional member access operator (`->`) as part of the token.
 	// 
 	// Note: Token prefixes ('@', '#', '$') are handled by the caller, so scanning begins after the prefix (`start = 1`).
@@ -3767,7 +3791,9 @@ static size_t FindResourceCopyTargetTokenEnd(const std::wstring& str, size_t sta
 
 	NamespaceScanner namespace_scanner;
 
-	for (size_t i = start; i < str.size(); ++i)
+	size_t end = str.size();
+
+	for (size_t i = start; i < end; ++i)
 	{
 		wchar_t c = str[i];
 
@@ -3803,25 +3829,31 @@ static size_t FindResourceCopyTargetTokenEnd(const std::wstring& str, size_t sta
 			continue;
 		}
 
-		// Allow the member access operator ("->") as part of the token.
 		if (c == L'-')
 		{
-			if (i + 1 < str.size() && str[i + 1] == L'>')
+			// Allow the member access operator ("->") as part of the token.
+			if (i + 1 < end && str[i + 1] == L'>')
 			{
 				++i; // Consume '>'.
 				continue;
 			}
+			// Allow hyphen as part of identifier.
+			continue;
 		}
 
-		// The remaining characters must be valid identifier characters.
-		if (!is_identifier_char(c, OptionalChars(OptionalChars::HYPHEN | OptionalChars::PERIOD)))
-			return i;
+		// The remaining characters must be non-operator characters.
+		// Spacing character outside namespace terminates a token.
+		if (is_operator_char(c) || iswspace(c))
+		{
+			end = i;
+			break;
+		}
 	}
 
 	if (!brackets.empty())
-		throw CommandListSyntaxError(L"Unterminated bracket expression", str.size());
+		throw CommandListSyntaxError(L"Unterminated bracket expression", end);
 
-	return str.size();
+	return end;
 }
 
 inline bool ParseFloatToken(const wstring& input, float& out, size_t& length)
@@ -4447,32 +4479,26 @@ static void tokenise(const wstring* expression, CommandListSyntaxTree* tree, con
 
 		bool has_prefix = has_variable_prefix || remain[0] == L'@' || remain[0] == L'#';
 
-		// ResourceCopyTarget
-		{
-			size_t len = FindResourceCopyTargetTokenEnd(remain, has_prefix ? 1 : 0);
-
-			if (len)
-			{
-				token = remain.substr(0, len);
-
-				if (operand->parse_target( &token, ini_namespace, scope))
-				{
-					LogDebugW(L"      ResourceCopyTarget: \"%ls\"\n", token.c_str());
-					pos += len;
-					goto import_operand;
-				}
-			}
-		}
+		size_t len = 0;
 
 		// Other Tokens
 		if (!has_prefix)
 		{
-			size_t len = FindIdentifierTokenEnd(remain, 0, OptionalChars::NONE);
+			len = FindIdentifierTokenEnd(remain, 0, OptionalChars::NONE);
 
 			if (len)
 			{
 				token = remain.substr(0, len);
 
+				// Parse target without hyphen (e.g. `ib`, `vb0`).
+				if (operand->parse_slot(&token, ini_namespace, scope))
+				{
+					LogDebug("      ResourceCopyTarget: \"%S\"\n", token.c_str());
+					pos += len;
+					goto import_operand;
+				}
+
+				// Parse ini params (e.g. `x`, `y0`, `z128`).
 				if (operand->parse_ini_param(&token, ini_namespace, scope))
 				{
 					LogDebug("      IniParam: \"%S\"\n", token.c_str());
@@ -4480,21 +4506,25 @@ static void tokenise(const wstring* expression, CommandListSyntaxTree* tree, con
 					goto import_operand;
 				}
 
-				else if (operand->parse_ini_keywords(&token, ini_namespace, scope))
+				// Parse INI "ParamOverride" value getters (e.g. `TIME`, `INDEX_COUNT`).
+				if (operand->parse_ini_keywords(&token, ini_namespace, scope))
 				{
 					LogDebug("      IniKeyword: \"%S\"\n", token.c_str());
 					pos += len;
 					goto import_operand;
 				}
 
-				else if (operand->parse_shader(&token, ini_namespace, scope))
+				// Parse special float (e.g. `inf`, `NaN`). Hyphen before `inf` is handled by operator.
+				if (operand->parse_float(&remain, ini_namespace, scope, len))
 				{
-					LogDebug("      Shader: \"%S\"\n", token.c_str());
+					token = remain.substr(0, len);
+					LogDebug("      Float: \"%S\"\n", token.c_str());
 					pos += len;
 					goto import_operand;
 				}
 
-				else if (operand->parse_scissor(&token, ini_namespace, scope))
+				// Parse scissor (e.g. `scissor0_top`).
+				if (operand->parse_scissor(&token, ini_namespace, scope))
 				{
 					LogDebug("      Scissor: \"%S\"\n", token.c_str());
 					pos += len;
@@ -4502,15 +4532,32 @@ static void tokenise(const wstring* expression, CommandListSyntaxTree* tree, con
 				}
 			}
 		}
-
-		// Special Float:
+		
+		// More loose match with hyphens, brackets and UTF-8.
+		// Allows strings like `Pool\path like\namespace\chars_UTF-8[$index]->Call($PoolFoo[$index], 1)`.
+		size_t len_target = FindResourceCopyTargetTokenEnd(remain, has_prefix ? 1 : 0);
+		if (len_target)
 		{
-			size_t len = remain.size();
+			token = remain.substr(0, len_target);
 
-			if (operand->parse_float(&remain, ini_namespace, scope, len))
+			// Parse custom resource, pool or other target (e.g. `ResourceFoo`, `PoolFoo`, `cs-cb0`, `ib`).
+			if (operand->parse_target(&token, ini_namespace, scope))
 			{
-				token = remain.substr(0, len);
-				LogDebug("      Float: \"%S\"\n", token.c_str());
+				LogDebugW(L"      ResourceCopyTarget: \"%ls\"\n", token.c_str());
+				pos += len_target;
+				goto import_operand;
+			}
+		}
+
+		// Must be attempted after target, otherwise it'll win over slots (e.g. `vs` over `vs-cb0`).
+		if (!has_prefix && len)
+		{
+			token = remain.substr(0, len);
+
+			// Parse shader (e.g. `vs`, `cs`).
+			if (operand->parse_shader(&token, ini_namespace, scope))
+			{
+				LogDebug("      Shader: \"%S\"\n", token.c_str());
 				pos += len;
 				goto import_operand;
 			}
@@ -5315,6 +5362,17 @@ bool CommandListOperand::parse_variable(const wstring* operand, const wstring* i
 	return false;
 }
 
+bool CommandListOperand::parse_slot(const wstring* operand, const wstring* ini_namespace, CommandListScope* scope)
+{
+	int ret;
+	ret = texture_filter_target.ParseTarget(operand->c_str(), true, ini_namespace, scope, false);
+	if (ret) {
+		type = ParamOverrideType::TEXTURE;
+		return operand_allowed_in_context(type, scope);
+	}
+	return false;
+}
+
 bool CommandListOperand::parse_target(const wstring* operand, const wstring* ini_namespace, CommandListScope* scope)
 {
 	int ret;
@@ -5459,7 +5517,14 @@ bool ParseCommandListVariableAssignment(const wchar_t *section,
 	CommandListVariable* var = nullptr;
 
 	if (!args.GetVariable(var, false, CommandArgumentReader::PeekMode::Argument))
-		return args.Fail();
+	{
+		// Report only "locked" variable error for now to avoid `d3dx_user.ini` error spam.
+		// TODO: Refactor syntax parsing errors reporting.
+		if (var->flags & VariableFlags::LOCKED)
+			return args.Fail();
+		else
+			return false;
+	}
 
 	VariableAssignment* command = new VariableAssignment();
 
@@ -7568,7 +7633,7 @@ bool contains_whitespace(const wchar_t* str, size_t len)
 	return false;
 }
 
-bool ResourceCopyTarget::ParseTarget(const wchar_t *target, bool is_source, const wstring *ini_namespace, CommandListScope* scope)
+bool ResourceCopyTarget::ParseTarget(const wchar_t *target, bool is_source, const wstring *ini_namespace, CommandListScope* scope, bool allow_custom)
 {
 	IniParserResult ret;
 	size_t length = wcslen(target);
@@ -7577,38 +7642,41 @@ bool ResourceCopyTarget::ParseTarget(const wchar_t *target, bool is_source, cons
 	if (!target || length < 2)
 		return false;
 
-	// Consume an optional target prefix (`@` or `#` or `$`).
-	ret = ParseTargetPrefix(target, length);
-	//LogInfo("ParseTarget: %d at ParseTargetPrefix\n", ret);
-	if (ret == IniParserResult::SYNTAX_ERROR)
-		return false;
-
-	// Parse pool variable early.
-	if (evaluation_mode == ResourceCopyTargetEvaluationMode::VARIABLE)
+	if (allow_custom)
 	{
-		// Parse pool variable (e.g. `$PoolFoo[0]`).
+		// Consume an optional target prefix (`@` or `#` or `$`).
+		ret = ParseTargetPrefix(target, length);
+		//LogInfo("ParseTarget: %d at ParseTargetPrefix\n", ret);
+		if (ret == IniParserResult::SYNTAX_ERROR)
+			return false;
+
+		// Parse pool variable early.
+		if (evaluation_mode == ResourceCopyTargetEvaluationMode::VARIABLE)
+		{
+			// Parse pool variable (e.g. `$PoolFoo[0]`).
+			ret = ParseTargetPool(target, length, ini_namespace, scope, is_source);
+			//LogInfo("ParseTarget: %d at ParseTargetPool\n", ret);
+			return ret == IniParserResult::TOKEN_FOUND;
+		}
+
+		// Consume an optional resource member suffix (e.g. `->HashRegion(0, 16)` or `->Length`).
+		ret = ParseTargetMember(target, length, temp_target, ini_namespace, scope);
+		//LogInfo("ParseTarget: %d at ParseTargetMember\n", ret);
+		if (ret == IniParserResult::SYNTAX_ERROR)
+			return false;
+
+		// Parse the remainder as a custom resource (e.g. `ResourceFoo`).
+		ret = ParseTargetCustomResource(target, length, ini_namespace, scope);
+		//LogInfo("ParseTarget: %d at ParseTargetCustomResource\n", ret);
+		if (ret != IniParserResult::TOKEN_NOT_FOUND)
+			return ret == IniParserResult::TOKEN_FOUND;
+
+		// Parse the remainder as a resource pool (e.g. `PoolFoo`).
 		ret = ParseTargetPool(target, length, ini_namespace, scope, is_source);
 		//LogInfo("ParseTarget: %d at ParseTargetPool\n", ret);
-		return ret == IniParserResult::TOKEN_FOUND;
+		if (ret != IniParserResult::TOKEN_NOT_FOUND)
+			return ret == IniParserResult::TOKEN_FOUND;
 	}
-
-	// Consume an optional resource member suffix (e.g. `->HashRegion(0, 16)` or `->Length`).
-	ret = ParseTargetMember(target, length, temp_target, ini_namespace, scope);
-	//LogInfo("ParseTarget: %d at ParseTargetMember\n", ret);
-	if (ret == IniParserResult::SYNTAX_ERROR)
-		return false;
-
-	// Parse the remainder as a custom resource (e.g. `ResourceFoo`).
-	ret = ParseTargetCustomResource(target, length, ini_namespace, scope);
-	//LogInfo("ParseTarget: %d at ParseTargetCustomResource\n", ret);
-	if (ret != IniParserResult::TOKEN_NOT_FOUND)
-		return ret == IniParserResult::TOKEN_FOUND;
-
-	// Parse the remainder as a resource pool (e.g. `PoolFoo`).
-	ret = ParseTargetPool(target, length, ini_namespace, scope, is_source);
-	//LogInfo("ParseTarget: %d at ParseTargetPool\n", ret);
-	if (ret != IniParserResult::TOKEN_NOT_FOUND)
-		return ret == IniParserResult::TOKEN_FOUND;
 
 	// Parse the remainder as a pipeline slot (e.g. `vb0`, `this`, `null`).
 	ret = ParseTargetPipelineSlot(target, length, is_source);

DirectX11/CommandList.h
@@ -881,7 +881,7 @@ public:
 
 	bool forbid_view_cache = false;
 
-	bool ParseTarget(const wchar_t *target, bool is_source, const wstring *ini_namespace, CommandListScope* scope);
+	bool ParseTarget(const wchar_t *target, bool is_source, const wstring *ini_namespace, CommandListScope* scope, bool allow_custom = true);
 
 	void SetCustomResource(CustomResource* resource);
 
@@ -1315,6 +1315,7 @@ public:
 	bool parse_float(const wstring* operand, const wstring* ini_namespace, CommandListScope* scope, size_t& out_length);
 	bool parse_ini_param(const wstring* operand, const wstring* ini_namespace, CommandListScope* scope);
 	bool parse_variable(const wstring* operand, const wstring* ini_namespace, CommandListScope* scope);
+	bool parse_slot(const wstring* operand, const wstring* ini_namespace, CommandListScope* scope);
 	bool parse_target(const wstring* operand, const wstring* ini_namespace, CommandListScope* scope);
 	bool parse_shader(const wstring* operand, const wstring* ini_namespace, CommandListScope* scope);
 	bool parse_scissor(const wstring* operand, const wstring* ini_namespace, CommandListScope* scope);

commit fdab51e2f26438a3e9ea6b3dcc34c5a84de550f5

Fixed square and round brackets being wrongly treated as part of identifier
Regression was caused by commit 9fc83bc.

DirectX11/CommandList.cpp
@@ -3657,6 +3657,10 @@ static inline bool is_operator_char(wchar_t c)
 	case L'!':
 	case L'^':
 	case L'~':
+	case L'(':
+	case L')':
+	case L'[':
+	case L']':
 		return true;
 
 	default: